204156536e96f2bca799025348082b7a660284f2
FAITH <OnlyMeHipnoz@hotmail.com>
2019-11-10 00:21:10 +0700

    live graph plotting

---
+++ plotcanvas.py
@@ -0,0 +1,41 @@
+import sys
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+from PyQt5.QtCore import QTimer
+from PyQt5 import QtCore
+import random
+import sqlite3
+
+class Canvas(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        fig.set_facecolor('lightgrey')
+        self.axes = fig.add_subplot(111)
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+class MyDynamicMplCanvas(Canvas):
+    """A canvas that updates itself every second with a new plot."""
+
+    def __init__(self, *args, **kwargs):
+        Canvas.__init__(self, *args, **kwargs)
+        timer = QtCore.QTimer(self)
+        self.conn = sqlite3.connect('test.db')
+        self.cur = self.conn.cursor()
+        timer.timeout.connect(self.plot)
+        timer.start(1000)
+
+
+
+    def plot(self):
+        labels = []
+        value = []
+        data = self.cur.execute("select * from items")
+        for i in data:
+            labels.append(i[0])
+            value.append(i[1])
+        print(labels, value)
+        ax = self.figure.add_subplot(111)
+        ax.cla()
+        ax.pie(value, labels=labels)
+        self.draw()
