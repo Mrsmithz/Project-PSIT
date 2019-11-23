6aa45d3363b4bb57ab501b6950a86fd2f2de10a1
FAITH <OnlyMeHipnoz@hotmail.com>
2019-11-23 22:12:32 +0700

    add live graphs plotting circle function

--- plotcanvas.py
+++ plotcanvas.py
@@ -5,6 +5,7 @@ from PyQt5.QtCore import QTimer
 from PyQt5 import QtCore
 import random
 import sqlite3
+import time
 
 class Canvas(FigureCanvas):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
@@ -16,19 +17,16 @@ class Canvas(FigureCanvas):
 
 class MyDynamicMplCanvas(Canvas):
     """A canvas that updates itself every second with a new plot."""
-
     def __init__(self, *args, **kwargs):
         Canvas.__init__(self, *args, **kwargs)
         self.conn = sqlite3.connect('test.db')
         self.cur = self.conn.cursor()
-        self.plot()
-        timer = QtCore.QTimer(self)
-        timer.timeout.connect(self.plot)
-        timer.start(1000)
-
-
-
-    def plot(self):
+        self.plot_by_price()
+        #timer = QtCore.QTimer(self)
+        #timer.timeout.connect(self.liveplot)
+        #timer.start(1000)
+        self.liveplot()
+    def plot_by_price(self):
         labels = []
         value = []
         data = self.cur.execute("select * from items")
@@ -38,4 +36,50 @@ class MyDynamicMplCanvas(Canvas):
         ax = self.figure.add_subplot(111)
         ax.cla()
         ax.pie(value, labels=labels, autopct="%1.1f%%", shadow=True)
+        ax.set_title("Pie Chart By Price")
         self.draw()
+
+    def plot_by_quantity(self):
+        labels = []
+        value = []
+        data = self.cur.execute("select * from items")
+        for i in data:
+            labels.append(i[0])
+            value.append(i[2])
+        ax = self.figure.add_subplot(111)
+        ax.cla()
+        ax.pie(value, labels=labels, autopct="%1.1f%%", shadow=True)
+        ax.set_title("Pie Chart By Quantity")
+        self.draw()
+
+    def plot_by_rate(self):
+        labels = []
+        value = []
+        data = self.cur.execute("select * from items")
+        for i in data:
+            labels.append(i[0])
+            value.append(i[4])
+        ax = self.figure.add_subplot(111)
+        ax.cla()
+        ax.pie(value, labels=labels, autopct="%1.1f%%", shadow=True)
+        ax.set_title("Pie Chart By Rate")
+        self.draw()
+
+    def liveplot(self):
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.plot_by_price)
+        timer.timeout.connect(self.liveplot2)
+        timer.setSingleShot(True)
+        timer.start(1000)
+    def liveplot2(self):
+        timer2 = QtCore.QTimer(self)
+        timer2.timeout.connect(self.plot_by_quantity)
+        timer2.timeout.connect(self.liveplot3)
+        timer2.setSingleShot(True)
+        timer2.start(1000)
+    def liveplot3(self):
+        timer3 = QtCore.QTimer(self)
+        timer3.timeout.connect(self.plot_by_rate)
+        timer3.timeout.connect(self.liveplot)
+        timer3.setSingleShot(True)
+        timer3.start(1000)
