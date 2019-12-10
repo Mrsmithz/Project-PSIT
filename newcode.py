561049205250c522eb2b8c292e320ace86a733a6
FAITH <OnlyMeHipnoz@hotmail.com>
2019-12-11 00:31:21 +0700

    add styling and fix some code

--- newcode.py
+++ newcode.py
@@ -37,35 +37,42 @@ class MainWindow(QMainWindow):
         self.cur = self.conn.cursor()
 
         self.main = QWidget(self)
+        self.main.setStyleSheet(f'background-image: url({path}/img/bg2.jpg);')
         font = QtGui.QFont()
-        font.setPointSize(12) # set the point size
+        font.setPointSize(14) # set the point size
 
         self.clearbtn = QPushButton(self.main) # set variable clear button
         self.clearbtn.setGeometry(250, 620, 111, 51) # set where the button is
         self.clearbtn.setText("Clear") # set the name of button --> "Clear"
+        self.clearbtn.setFont(font)
         self.clearbtn.clicked.connect(self.clear) # if the button was clicked, it will connecting to "clear" function
 
         self.submitbtn = QPushButton(self.main) # set variable --> submit button
         self.submitbtn.setGeometry(10, 620, 111, 51) # set where the button is
         self.submitbtn.setText("Submit") # set the name of the button --> "Submit"
+        self.submitbtn.setFont(font)
         self.submitbtn.clicked.connect(self.submit) # if the button was clicked, it will connecting to "submit" function
 
         self.deletebtn = QPushButton(self.main) # set variable --> delete button
         self.deletebtn.setGeometry(130, 620, 111, 51) #set where the button is
         self.deletebtn.setText("Delete") # set the name of the button --> "Delete"
+        self.deletebtn.setFont(font)
         self.deletebtn.clicked.connect(self.delete) # if the button was clicked, it will connecting to "delete" function
 
         self.name_input = QLineEdit(self.main) # set variable --> input() of name
         self.name_input.setGeometry(90, 230, 291, 41) # set where the name input is
         self.name_input.setFont(font) # set the size of the letter that you will going to input
+        self.name_input.setStyleSheet(f'background-image: url({path}/img/test.jpg);')
 
         self.price_input = QLineEdit(self.main) # set variable --> input() of price
         self.price_input.setGeometry(90, 280, 291, 41) # set where the  price input is
         self.price_input.setFont(font) # set the size of the letter that you will going to input
+        self.price_input.setStyleSheet(f'background-image: url({path}/img/test.jpg);')
 
         self.quantity_input = QLineEdit(self.main) # set variable --> input() of quantity
         self.quantity_input.setGeometry(90, 330, 291, 41) # set where the quantity input is
         self.quantity_input.setFont(font) # set the size of the letter that you will going to input
+        self.quantity_input.setStyleSheet(f'background-image: url({path}/img/test.jpg);')
 
         self.date_input = QDateEdit(self.main) # set variable --> input() of date
         self.date_input.setGeometry(90, 380, 91, 21) # set where the date input is
@@ -77,10 +84,12 @@ class MainWindow(QMainWindow):
         self.rate_input = QLineEdit(self.main) # set variable --> input() of rate
         self.rate_input.setGeometry(90, 420, 171, 41) # set where the input() is
         self.rate_input.setFont(font) # set the size of the letter that you will going to input
+        self.rate_input.setStyleSheet(f'background-image: url({path}/img/test.jpg);')
 
-        self.note_input = QLineEdit(self.main) # set variable --> input() of note
+        self.note_input = QTextEdit(self.main) # set variable --> input() of note
         self.note_input.setGeometry(90, 470, 291, 131) # set where the input() is
         self.note_input.setFont(font) # set the size of the letter that you will going to input
+        self.note_input.setStyleSheet(f'background-image: url({path}/img/test.jpg);')
 
         self.mytable = QTableWidget(self.main) # set the table
         self.mytable.setEditTriggers(QtWidgets.QAbstractItemView.DoubleClicked) # set edittriggers for mytable
@@ -89,36 +98,43 @@ class MainWindow(QMainWindow):
         self.mytable.setGeometry(10, 10, 1001, 211) # set where the table is?
         self.mytable.doubleClicked.connect(self.savefromtable) # connect to function when doubleclicked
         self.mytable.cellPressed.connect(self.savefromtable) # connect to function when get cellpressed
+        self.mytable.setStyleSheet(f'background-image: url({path}/img/test.jpg);')
 
         self.name_label = QLabel(self.main) # set the variable --> label of "name"
         self.name_label.setGeometry(10, 240, 61, 16) # set where the "name" label is
         self.name_label.setFont(font) # set a font
         self.name_label.setText("Name") # called this label as "Name"
+        self.name_label.setStyleSheet('background:transparent;')
 
         self.price_label = QLabel(self.main) # set the variable --> label of "price"
         self.price_label.setGeometry(10, 290, 51, 16) # set where the "price" label is
         self.price_label.setFont(font) # set a font
         self.price_label.setText("Price") # called this label as "Price"
+        self.price_label.setStyleSheet('background:transparent;')
 
         self.quantity_label = QLabel(self.main) # set the variable --> label of "price"
         self.quantity_label.setGeometry(10, 343, 81, 20)  # set where the "price" label is
         self.quantity_label.setFont(font) # set a font
         self.quantity_label.setText("Quantity") # called this label as "Quantity"
+        self.quantity_label.setStyleSheet('background:transparent;')
 
         self.date_label = QLabel(self.main) # set the variable --> label of "price"
         self.date_label.setGeometry(10, 380, 101, 21)  # set where the "price" label is
         self.date_label.setFont(font) # set a font
         self.date_label.setText("Date") # called this label as "Date"
+        self.date_label.setStyleSheet('background:transparent;')
 
         self.rate_label = QLabel(self.main) # set the variable --> label of "price"
         self.rate_label.setGeometry(10, 430, 111, 21)  # set where the "price" label is
         self.rate_label.setFont(font) # set a font
         self.rate_label.setText("Rate") # called this label as "Rate"
+        self.rate_label.setStyleSheet('background:transparent;')
 
         self.note_label = QLabel(self.main) # set the variable --> label of "price"
         self.note_label.setGeometry(10, 520, 81, 21)  # set where the "price" label is
         self.note_label.setFont(font) # set a font
         self.note_label.setText("Note") # called this label as "Note"
+        self.note_label.setStyleSheet('background:transparent;')
 
         self.status_bar = QStatusBar(self) # use the status bar that you import from the beginning
         self.setStatusBar(self.status_bar)
@@ -151,6 +167,7 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.main)
 
         self.plotpie = QWidget(self.main)
+        self.plotpie.setStyleSheet('background:transparent;')
         self.plotpie.setGeometry(400, 220, 611, 471)
         self.pie_box = QVBoxLayout(self.plotpie)
         self.pie = MyDynamicMplCanvas(self.plotpie)
@@ -168,7 +185,7 @@ class MainWindow(QMainWindow):
             return
         date = str(self.date_input.date().toPyDate())
         date = "-".join((date[8:10], date[5:7], date[0:4]))
-        notes = self.note_input.text()
+        notes = self.note_input.toPlainText()
         self.cur.execute("insert into items values (?, ?, ?, ?, ?, ?)", (name, price, quantity, date, monthly, notes))
         self.conn.commit()
         list1 = self.cur.execute("select * from items")
