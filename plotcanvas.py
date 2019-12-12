5e907f34a1834f70c23e2071d8f48590fc1082a1
FAITH <OnlyMeHipnoz@hotmail.com>
2019-12-13 00:09:25 +0700

    add docstring to all function

--- plotcanvas.py
+++ plotcanvas.py
@@ -10,6 +10,7 @@ import numpy as np
 
 
 class Canvas(FigureCanvas):
+    """this class will be a box that you'll plotting any graph in to it"""
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
         fig.set_facecolor('lightgrey')
@@ -21,14 +22,12 @@ class MyDynamicMplCanvas(Canvas):
     """A canvas that updates itself every second with a new plot."""
     def __init__(self, *args, **kwargs):
         Canvas.__init__(self, *args, **kwargs)
-        self.conn = sqlite3.connect('test.db')
+        self.conn = sqlite3.connect('data.db')
         self.cur = self.conn.cursor()
         self.plot_by_price()
-        #timer = QtCore.QTimer(self)
-        #timer.timeout.connect(self.liveplot)
-        #timer.start(1000)
         self.liveplot()
     def plot_by_price(self):
+        """Plot the new pie by price of items in database"""
         labels = []
         value = []
         data = self.cur.execute("select * from items")
@@ -42,6 +41,7 @@ class MyDynamicMplCanvas(Canvas):
         self.draw()
 
     def plot_by_quantity(self):
+        """Plot the new pie by quantity of items in database"""
         labels = []
         value = []
         data = self.cur.execute("select * from items")
@@ -55,6 +55,7 @@ class MyDynamicMplCanvas(Canvas):
         self.draw()
 
     def plot_by_rate(self):
+        """Plot the new pie by monthly rate of items in database"""
         labels = []
         value = []
         data = self.cur.execute("select * from items")
@@ -67,62 +68,66 @@ class MyDynamicMplCanvas(Canvas):
         ax.set_title("Pie Chart By Rate")
         self.draw()
 
-    def liveplot(self):
+    def liveplot(self): # this three function will rotate the pie plot every 1 sec
+        """this function will triggered liveplot2 after 1 sec"""
         timer = QtCore.QTimer(self)
         timer.timeout.connect(self.plot_by_price)
         timer.timeout.connect(self.liveplot2)
         timer.setSingleShot(True)
         timer.start(1000)
     def liveplot2(self):
+        """this function will triggered liveplot3 after 1 sec"""
         timer2 = QtCore.QTimer(self)
         timer2.timeout.connect(self.plot_by_quantity)
         timer2.timeout.connect(self.liveplot3)
         timer2.setSingleShot(True)
         timer2.start(1000)
     def liveplot3(self):
+        """this function will triggered liveplot after 1 sec"""
         timer3 = QtCore.QTimer(self)
         timer3.timeout.connect(self.plot_by_rate)
         timer3.timeout.connect(self.liveplot)
         timer3.setSingleShot(True)
         timer3.start(1000)
 
-class barplot(Canvas):
-    def __init__(self, *args, **kwargs):
-        Canvas.__init__(self, *args, **kwargs)
-        self.conn = sqlite3.connect('test.db')
-        self.cur = self.conn.cursor()
-        self.get_data()
-    def get_data(self):
-        self.labels = []
-        self.price = []
-        self.quantity = []
-        self.rate = []
-        self.ax = self.figure.add_subplot(111)
-        data = self.cur.execute('select * from items')
-        for i in data:
-            self.labels.append(i[0])
-            self.price.append(float(i[1]))
-            self.quantity.append(int(i[2]))
-            self.rate.append(float("%.2f"%float(i[4])))
-        x = np.arange(len(self.labels))
-        width = 0.25
-        rect1 = self.ax.bar(x + 0.00, self.price, width, label="Price")
-        rect2 = self.ax.bar(x + 0.25, self.quantity, width, label="Quantity")
-        rect3 = self.ax.bar(x + 0.50, self.rate, width, label="rate")
-        self.ax.set_ylabel('TEST')
-        self.ax.set_title('TEST')
-        self.ax.set_xticks(x)
-        self.ax.set_xticklabels(self.labels)
-        self.ax.legend()
-        self.autolabel(rect1)
-        self.autolabel(rect2)
-        self.autolabel(rect3)
-        self.draw()
-    def autolabel(self, rects):
-        for rect in rects:
-            height = rect.get_height()
-            self.ax.annotate('{}'.format(height),
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(0, 3),  # 3 points vertical offset
-                    textcoords="offset points",
-                    ha='center', va='bottom')
+# class barplot(Canvas):
+#     """this class will plot a barplot but we didn't use it right now"""
+#     def __init__(self, *args, **kwargs):
+#         Canvas.__init__(self, *args, **kwargs)
+#         self.conn = sqlite3.connect('test.db')
+#         self.cur = self.conn.cursor()
+#         self.get_data()
+#     def get_data(self):
+#         self.labels = []
+#         self.price = []
+#         self.quantity = []
+#         self.rate = []
+#         self.ax = self.figure.add_subplot(111)
+#         data = self.cur.execute('select * from items')
+#         for i in data:
+#             self.labels.append(i[0])
+#             self.price.append(float(i[1]))
+#             self.quantity.append(int(i[2]))
+#             self.rate.append(float("%.2f"%float(i[4])))
+#         x = np.arange(len(self.labels))
+#         width = 0.25
+#         rect1 = self.ax.bar(x + 0.00, self.price, width, label="Price")
+#         rect2 = self.ax.bar(x + 0.25, self.quantity, width, label="Quantity")
+#         rect3 = self.ax.bar(x + 0.50, self.rate, width, label="rate")
+#         self.ax.set_ylabel('TEST')
+#         self.ax.set_title('TEST')
+#         self.ax.set_xticks(x)
+#         self.ax.set_xticklabels(self.labels)
+#         self.ax.legend()
+#         self.autolabel(rect1)
+#         self.autolabel(rect2)
+#         self.autolabel(rect3)
+#         self.draw()
+#     def autolabel(self, rects):
+#         for rect in rects:
+#             height = rect.get_height()
+#             self.ax.annotate('{}'.format(height),
+#                     xy=(rect.get_x() + rect.get_width() / 2, height),
+#                     xytext=(0, 3),  # 3 points vertical offset
+#                     textcoords="offset points",
+#                     ha='center', va='bottom')
